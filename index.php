023d5da0cc5d08d44a7032ec8d11557c09ecc646
smugford <scott.w.mugford@gmail.com>
2022-12-31 15:55:58 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+  <H1>The Cloud Brothers Default Demo Content</H1>
+  
+  <p> Simple PHP Page that displays the date and day of the week </p>
+  
+<?php
+echo "Today is " . date("Y-m-d") . "<br>";
+echo "Today is " . date("l");
+?>
+
+</body>
+</html>
